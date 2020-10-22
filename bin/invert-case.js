ec0b481b129ab0487bb5debc1cc3a45a4c9906b8
LainVellio <cvellio@gmail.com>
2020-10-22 12:05:47 +0300

    add invertCase

---
+++ bin/invert-case.js
@@ -0,0 +1,9 @@
+import promptly from 'promptly';
+import { invertCase } from '../src/invertCase.js';
+
+const launchGame = async () => {
+  const value = await promptly.prompt('Input string ');
+  const result = invertCase(value);
+  return console.log(result);
+};
+launchGame();
---
+++ src/invertCase.js
@@ -0,0 +1,11 @@
+export const invertCase = (str) => {
+  let result = '';
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === str[i].toUpperCase()) {
+      result += str[i].toLowerCase();
+    } else result += str[i].toUpperCase();
+  }
+  return result;
+};
+
+export default { invertCase };
