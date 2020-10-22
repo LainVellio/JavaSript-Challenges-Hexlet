e41507c160b967b38e93904a22bdece17b627daa
LainVellio <cvellio@gmail.com>
2020-10-22 12:52:46 +0300

    add diff

---
+++ bin/diff.js
@@ -0,0 +1,10 @@
+import promptly from 'promptly';
+import { diff } from '../src/diff.js';
+
+const launchGame = async () => {
+  const firstCorner = await promptly.prompt('Input first corner ');
+  const secondCorner = await promptly.prompt('Input second corner');
+  const result = diff(firstCorner, secondCorner);
+  return console.log(result);
+};
+launchGame();
---
+++ src/diff.js
@@ -0,0 +1,11 @@
+export const diff = (firstCorner, secondCorner) => {
+  if (firstCorner < 0 || firstCorner > 360 || secondCorner < 0 || secondCorner > 360) {
+    return 'Wrong value';
+  }
+  const diffCorner = secondCorner - firstCorner;
+  if (diffCorner <= 180) {
+    return diffCorner;
+  } return 360 - diffCorner;
+};
+
+export default { diff };
