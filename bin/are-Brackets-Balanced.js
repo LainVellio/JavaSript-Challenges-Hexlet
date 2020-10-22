2eaab170500e48ec119f25d9e462e9716470d6c1
LainVellio <cvellio@gmail.com>
2020-10-22 14:06:50 +0300

    add are brackets balanced

---
+++ bin/are-Brackets-Balanced.js
@@ -0,0 +1,9 @@
+import promptly from 'promptly';
+import { areBracketsBalanced } from '../src/areBracketsBalanced.js';
+
+const launchGame = async () => {
+  const string = await promptly.prompt('Input string ', { default: ' ' });
+  const result = areBracketsBalanced(string);
+  return console.log(result);
+};
+launchGame();
---
+++ src/areBracketsBalanced.js
@@ -0,0 +1,13 @@
+export const areBracketsBalanced = (str) => {
+  if (str.replace(' ', '') === '') {
+    return true;
+  }
+  for (let i = 0; i < str.length / 2; i += 1) {
+    if ((str[i] !== '(') || (str[str.length - 1 - i] !== ')')) {
+      return false;
+    }
+  }
+  return true;
+};
+
+export default { areBracketsBalanced };
