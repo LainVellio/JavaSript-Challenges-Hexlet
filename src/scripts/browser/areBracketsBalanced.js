3d716dccf350ad656bfd5eb7c4d81fec2e90e133
LainVellio <cvellio@gmail.com>
2020-12-15 18:08:49 +0300

    add Are_Brackets_Balanced.html

--- src/scripts/browser/areBracketsBalanced.js
+++ src/scripts/browser/areBracketsBalanced.js
@@ -1,15 +1,12 @@
 // 5. Сбалансированы ли скобки?
 
 const areBracketsBalanced = (str) => {
-  if (str.replace(' ', '') === '') {
-    return true;
-  }
   for (let i = 0; i < str.length / 2; i += 1) {
     if ((str[i] !== '(') || (str[str.length - 1 - i] !== ')')) {
-      return false;
+      return 'Некорректная строка';
     }
   }
-  return true;
+  return 'Корректная строка';
 };
 
 // eslint-disable-next-line no-unused-vars
