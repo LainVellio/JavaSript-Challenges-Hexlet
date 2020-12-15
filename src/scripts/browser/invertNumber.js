b5c5828f9f17270c962dcc96d12c35672790fd32
LainVellio <cvellio@gmail.com>
2020-12-15 18:21:55 +0300

    add Invert_Number.html

--- src/scripts/browser/invertNumber.js
+++ src/scripts/browser/invertNumber.js
@@ -1,11 +1,24 @@
 // 6. Переворот числа
 
-export const invertNumber = (num) => {
+const invertNumber = (num) => {
+  if (Number.isNaN(Number(num))) {
+    return 'Вы ввели неверное значение';
+  }
+
+  if (num - Math.round(num) !== 0) {
+    return 'Вы ввели дробное число';
+  }
+
   let result = '';
   for (let i = 0; i < num.length; i += 1) {
     result += num[num.length - 1 - i];
   }
-  return Number(result);
+  return result;
 };
 
-export default { invertNumber };
+// eslint-disable-next-line no-unused-vars
+const getResult = () => {
+  const formInput = document.forms.program;
+  const a = formInput.a.value;
+  document.getElementById('result').textContent = invertNumber(a);
+};
