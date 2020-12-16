4fd56bb6bb9cf91652ecf04c7858bc0e87f705cb
LainVellio <cvellio@gmail.com>
2020-12-16 14:10:24 +0300

    add Ackermann_Function.html

--- src/scripts/browser/ackermannFunction.js
+++ src/scripts/browser/ackermannFunction.js
@@ -1,7 +1,19 @@
 // 8. Функция Аккермана
 
 // eslint-disable-next-line consistent-return
-export const ackermannFunction = (m, n) => {
+const ackermannFunction = (m, n) => {
+  if (Number.isNaN(m) || Number.isNaN(n)) {
+    return 'Вы ввели неверное значение';
+  }
+
+  if (m - Math.round(m) !== 0 || n - Math.round(n) !== 0) {
+    return 'Вы ввели дробное число';
+  }
+
+  if (m < 0 || n < 0) {
+    return 'Вы ввели отрицательное число';
+  }
+
   if (m > 0 && n > 0) {
     return ackermannFunction(m - 1, ackermannFunction(m, n - 1));
   }
@@ -13,4 +25,10 @@ export const ackermannFunction = (m, n) => {
   }
 };
 
-export default { ackermannFunction };
+// eslint-disable-next-line no-unused-vars
+const getResult = () => {
+  const formInput = document.forms.program;
+  const a = parseFloat(formInput.a.value);
+  const b = parseFloat(formInput.b.value);
+  document.getElementById('result').textContent = ackermannFunction(a, b);
+};
