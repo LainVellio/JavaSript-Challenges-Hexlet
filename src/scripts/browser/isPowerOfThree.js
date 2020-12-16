c9aaef8272607f5f3280149cc54f74c4a886a2e5
LainVellio <cvellio@gmail.com>
2020-12-16 16:05:06 +0300

    add Is_Power_Of_Three.html

--- src/scripts/browser/isPowerOfThree.js
+++ src/scripts/browser/isPowerOfThree.js
@@ -1,13 +1,30 @@
 // 9. Степень тройки
 
-export const isPowerOfThree = (num) => {
+const isPowerOfThree = (num) => {
+  if (Number.isNaN(num)) {
+    return 'Вы ввели неверное значение';
+  }
+
+  if (num - Math.round(num) !== 0) {
+    return 'Вы ввели дробное число';
+  }
+
+  if (num < 0) {
+    return 'Вы ввели отрицательное число';
+  }
+
   if (num === 3) {
-    return true;
+    return 'Да, является';
   }
   if (num % 3 === 0) {
     return isPowerOfThree(num / 3);
   }
-  return false;
+  return 'Нет, не является';
 };
 
-export default { isPowerOfThree };
+// eslint-disable-next-line no-unused-vars
+const getResult = () => {
+  const formInput = document.forms.program;
+  const a = parseFloat(formInput.a.value);
+  document.getElementById('result').textContent = isPowerOfThree(a);
+};
