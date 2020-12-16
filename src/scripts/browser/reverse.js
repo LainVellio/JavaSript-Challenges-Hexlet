628fda2482f3bc6353064fe3ebd4a31697ea303f
LainVellio <cvellio@gmail.com>
2020-12-16 15:29:10 +0300

    Reverse.html

--- src/scripts/browser/reverse.js
+++ src/scripts/browser/reverse.js
@@ -1,10 +1,15 @@
 // 14. Переворот строки
 
-export const reverse = (str) => {
+const reverse = (str) => {
   if (str.length === 1) {
     return str;
   }
   return str.slice(str.length - 1, str.length) + reverse(str.slice(0, str.length - 1));
 };
 
-export default { reverse };
+// eslint-disable-next-line no-unused-vars
+const getResult = () => {
+  const formInput = document.forms.program;
+  const a = formInput.a.value;
+  document.getElementById('result').textContent = reverse(a);
+};
