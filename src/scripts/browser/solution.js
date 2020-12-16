fcd091bbfad3c2cbdf52ff00e61635add82359f5
LainVellio <cvellio@gmail.com>
2020-12-16 15:54:22 +0300

    Solution.html

--- src/scripts/browser/solution.js
+++ src/scripts/browser/solution.js
@@ -39,13 +39,28 @@ const isTwoZero = (str) => {
   return true;
 };
 
-export const solution = (numZero, numOne) => {
+const solution = (numZero, numOne) => {
+  if (Number.isNaN(numZero) || Number.isNaN(numOne)) {
+    return 'Вы ввели неверное значение';
+  }
+
+  if (numZero - Math.round(numZero) !== 0 || numOne - Math.round(numOne) !== 0) {
+    return 'Вы ввели дробное число';
+  }
+
+  if (numZero < 0 || numOne < 0) {
+    return 'Вы ввели отрицательное число';
+  }
+
+  const result = [];
   const firstString = '1'.repeat(numOne) + '0'.repeat(numZero);
   const endString = '0'.repeat(numZero) + '1'.repeat(numOne);
   let currentStr = firstString;
   let counter = 0;
+  let i = 1;
 
-  console.log(currentStr);
+  result[i] = ` Все комбинации: ${currentStr}`;
+  i += 1;
   if (isTwoZero(currentStr)) {
     counter += 1;
   }
@@ -53,21 +68,31 @@ export const solution = (numZero, numOne) => {
   while (currentStr !== endString) {
     while (currentStr[currentStr.length - 1] === '0') {
       currentStr = shift(currentStr, indexRightOne(currentStr));
-      console.log(currentStr);
+      result[i] = ` ${currentStr}`;
+      i += 1;
       if (isTwoZero(currentStr)) {
         counter += 1;
       }
     }
+
     if (numOne === 1) { break; }
     const leftStr = currentStr.slice(0, indexCut(currentStr) + 1);
     const rightStr = currentStr.slice(indexCut(currentStr) + 1);
     currentStr = paste(shift(leftStr, indexRightOne(leftStr)), rightStr, indexRightOne(leftStr));
-    console.log(currentStr);
+    result[i] = ` ${currentStr}`;
+    i += 1;
     if (isTwoZero(currentStr)) {
       counter += 1;
     }
   }
-  return counter;
+  result[0] = ` Количество комбинаций без двух нулей подряд: ${counter}`;
+  return result;
 };
 
-export default { solution };
+// eslint-disable-next-line no-unused-vars
+const getResult = () => {
+  const formInput = document.forms.program;
+  const a = parseFloat(formInput.a.value);
+  const b = parseFloat(formInput.b.value);
+  document.getElementById('result').textContent = solution(a, b);
+};
