8a665130263f2b0a02fa82e2a0dbfa88bb723cd8
LainVellio <cvellio@gmail.com>
2020-12-16 13:50:23 +0300

    add Fizz_Buzz.html

--- src/scripts/browser/fizBuzz.js
+++ src/scripts/browser/fizBuzz.js
@@ -1,20 +1,44 @@
 // 7. Найди Fizz и Buzz
 
-export const fizzBuzz = (beginNum, endNum) => {
-  if (beginNum > endNum) {
-    return;
+const fizzBuzz = (beginNum, endNum) => {
+  if (Number.isNaN(beginNum) || Number.isNaN(endNum)) {
+    return 'Вы ввели неверное значение';
   }
+
+  if (beginNum - Math.round(beginNum) !== 0 || endNum - Math.round(endNum) !== 0) {
+    return 'Вы ввели дробное число';
+  }
+
+  if (beginNum < 0 || endNum < 0) {
+    return 'Вы ввели отрицательное число';
+  }
+
+  if (beginNum > endNum) {
+    return 'Начальное значение диапазона больше конечного';
+  }
+
+  const arr = [];
+  let a = 0;
   for (let i = beginNum; i <= endNum; i += 1) {
     if (i % 3 === 0 && i % 5 === 0) {
-      console.log('FizzBuzz');
+      arr[a] = ' FizzBuzz';
     } else if (i % 3 === 0) {
-      console.log('Fizz');
+      arr[a] = ' Fizz';
     } else if (i % 5 === 0) {
-      console.log('Buzz');
+      arr[a] = ' Buzz';
     } else {
-      console.log(i);
+      arr[a] = ` ${i}`;
     }
+    a += 1;
   }
+  // eslint-disable-next-line consistent-return
+  return arr;
 };
 
-export default { fizzBuzz };
+// eslint-disable-next-line no-unused-vars
+const getResult = () => {
+  const formInput = document.forms.program;
+  const a = parseFloat(formInput.a.value);
+  const b = parseFloat(formInput.b.value);
+  document.getElementById('result').textContent = fizzBuzz(a, b);
+};
